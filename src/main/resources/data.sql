e2594654325a080fd25ccadf8c61293880ea9a67
Ngozi Ofoefule <nojoechem@byteworks.com.ng>
2021-01-18 22:10:50 +0100

    project setup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,21 @@
 
-CREATE TABLE IF NOT EXISTS customer (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL
-);
 
-INSERT INTO customer (first_name, last_name) VALUES
-  ('Aliko', 'Dangote'),
-  ('Bill', 'Gates'),
-  ('Folrunsho', 'Alakija'),
-  ('Jeff', 'Bezos');
+INSERT INTO customer (id, first_name, last_name) VALUES
+  (1, 'Aliko', 'Dangote'),
+  (2, 'Bill', 'Gates'),
+  (3, 'Peter', 'Okorocha'),
+  (4, 'Jeff', 'Bezos');
 
 
-CREATE TABLE IF NOT EXISTS product (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  name VARCHAR(250) NOT NULL
-);
 
-INSERT INTO product (name) VALUES
-  ('Ferrari Spider'),
-  ('Range Rover Velar'),
-  ('Mercedes S Class'),
-  ('Rolls Royce Ghost');
-
-CREATE TABLE IF NOT EXISTS order_detail (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  customer_id int  NOT NULL,
-  product_id int NOT NULL
-);
-
-ALTER TABLE order_detail ADD FOREIGN KEY ( customer_id ) REFERENCES customer( id ) ;
-ALTER TABLE order_detail ADD FOREIGN KEY ( product_id ) REFERENCES product( id ) ;
+INSERT INTO product (id, name) VALUES
+  (1, 'Ferrari Spider'),
+  (2, 'Range Rover Velar'),
+  (3, 'Mercedes S Class'),
+  (4, 'Rolls Royce Ghost');
 
 
-INSERT INTO order_detail (customer_id, product_id) VALUES
-  (1,1),
-  (1,2);
+
+INSERT INTO order_detail (id, customer_id, product_id) VALUES
+  (1, 1,1),
+  (2, 1,2);
